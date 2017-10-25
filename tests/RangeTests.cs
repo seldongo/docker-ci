17a36382724e892f2be9207699c4b8dd9e2e03e9
Seldon Go <seldon.go@contactenergy.co.nz>
2017-10-26 11:18:00 +1300

    Test

--- tests/RangeTests.cs
+++ tests/RangeTests.cs
@@ -21,7 +21,7 @@ namespace tests
             var range = new Range { Count = 3, Sort = true };
             var values = new[] { "a", "c", "b" };
             var counter = 0;
-            var generated = range.Of(() => values[counter++]);
+            var generated = range.Of(() => values[counter++]); 
 
             Assert.Equal(new[] { "a", "b", "c" }, generated.ToArray());
         }
